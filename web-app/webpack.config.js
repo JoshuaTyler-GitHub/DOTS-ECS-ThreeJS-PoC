7e1f82752b8c070bce4c75bc6e68de58923a9279
Joshua Tyler <Joshua.4.Tyler@gmail.com>
2023-03-23 09:52:38 -0400

    source cleanup

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -2,7 +2,10 @@
 const fs = require('fs');
 
 // constants
-const REPLACEMENT_END_IDENTIFIER = String('        // Support React Native Web');
+const ENCODING = String('utf8');
+const REPLACEMENT_END_IDENTIFIER = String(
+  '        // Support React Native Web',
+);
 const REPLACEMENT_START_IDENTIFIER = String('      alias: {');
 
 // resources
@@ -11,7 +14,7 @@ const reactScriptsWebpackPath = String(
   'node_modules/react-scripts/config/webpack.config.js',
 );
 
-const getResolveAliases = () => {
+function getResolveAliases() {
   const aliases = {};
   const paths = jsConfigJson.compilerOptions.paths;
   const baseUrl = String(jsConfigJson.compilerOptions.baseUrl);
@@ -20,43 +23,53 @@ const getResolveAliases = () => {
     const pathValue = String(paths[pathKey])
       .replace('.', baseUrl)
       .replace('*', '');
-    const formattedPathValue = pathValue.replace('src/', '').substring(0, pathValue.length - 1);
-    const aliasValue = String(`path.resolve(paths.appSrc, '${formattedPathValue}')`);
+    const formattedPathValue = pathValue
+      .replace('src/', '')
+      .substring(0, pathValue.length - 1);
+    const aliasValue = String(
+      `path.resolve(paths.appSrc, '${formattedPathValue}')`,
+    );
     aliases[aliasKey] = aliasValue;
-    console.log(aliasValue);
   }
   return aliases;
-};
+}
 
-const addResolveAliases = () => {
-
+function addResolveAliases() {
   // create ReactScriptsWebpack aliases from jsconfig.json
   const aliasInserts = getResolveAliases();
-  const aliasReplacement = String([
-    `${REPLACEMENT_START_IDENTIFIER}`,
-    ...Object.entries(aliasInserts).map(([key, value]) => {
-      return `        '${key}': ${value},`;
-    })
+  const aliasReplacement = String(
+    [
+      `${REPLACEMENT_START_IDENTIFIER}`,
+      ...Object.entries(aliasInserts).map(([key, value]) => {
+        return `        '${key}': ${value},`;
+      }),
     ].join('\r\n') + '\r\n\r\n',
   );
 
   // read ReactScriptsWebpack
-  const reactScriptsWebpack = fs.readFileSync(reactScriptsWebpackPath, 'utf8');
-  const replacementStartIndex = reactScriptsWebpack.indexOf(REPLACEMENT_START_IDENTIFIER);
-  const replacementEndIndex = reactScriptsWebpack.indexOf(REPLACEMENT_END_IDENTIFIER);
+  const reactScriptsWebpack = fs.readFileSync(
+    reactScriptsWebpackPath,
+    ENCODING,
+  );
+  const replacementStartIndex = reactScriptsWebpack.indexOf(
+    REPLACEMENT_START_IDENTIFIER,
+  );
+  const replacementEndIndex = reactScriptsWebpack.indexOf(
+    REPLACEMENT_END_IDENTIFIER,
+  );
 
   // create new ReactScriptsWebpack
   const formattedReactScriptsWebpack = String(
     `${reactScriptsWebpack.substring(0, replacementStartIndex)}` +
-    `${aliasReplacement}` +
-    `${reactScriptsWebpack.substring(replacementEndIndex)}`
+      `${aliasReplacement}` +
+      `${reactScriptsWebpack.substring(replacementEndIndex)}`,
   );
 
   // write new ReactScriptsWebpack
   fs.writeFileSync(
     reactScriptsWebpackPath,
     formattedReactScriptsWebpack,
-    'utf-8',
+    ENCODING,
   );
-};
+}
 addResolveAliases();
